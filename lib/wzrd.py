919ecc255ca5c8ae057f3528a8e6467d6b34c669
Jacob Ives <jacob.w.ives@gmail.com>
2023-06-27 19:30:12 -0600

    add gold manager

--- lib/wzrd.py
+++ lib/wzrd.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from models import Spell, User, Character, Base
 import os
+import time
 
 
 def initialize():
@@ -43,12 +44,12 @@ def clear_terminal():
 
 def choose_user():
     clear_terminal()
-    print('Users:')
+    print('             Users:')
     user_list = session.query(User).all()
     valid_user_ids = []
     for user in user_list:
         valid_user_ids.append(user.user_id)
-        print(f'{user.user_id}) {user.username}')
+        print(f'            {user.user_id}) {user.username}')
     if valid_user_ids == []:
         print('No users exist')
         print('1) Create New User')
@@ -108,9 +109,9 @@ def user_menu(current_user_id):
 
     current_user = session.query(User).filter(User.user_id == int(current_user_id)).first()
     print(f'''
-        Welcome, {current_user.username}!
+                Welcome, {current_user.username}!
 
-
+               Choose a character:
     ''')
     character_query = session.query(Character).filter(Character.owner == current_user_id).all()
     valid_char_choices = []
@@ -120,7 +121,6 @@ def user_menu(current_user_id):
     if valid_char_choices != []:
         print('''
         -or-
-        
     ''')
     print('0) Create New Character')  
     choice = int(input ('Choose Character: '))
@@ -133,21 +133,57 @@ def user_menu(current_user_id):
         print('Not a valid response.')
 
 
-def character_menu(id, current_user_id):
+def character_menu(char_id, current_user_id):
     clear_terminal()
 
-    character = session.query(Character).filter(Character.character_id == id).first()
+    character = session.query(Character).filter(Character.character_id == char_id).first()
     print(f"""
           Character: {character.name}
           --------------------------------
           Level {character.level} | {character.gold} gp
 
-        0) Delete Character
+                    1) View Spellbook
+                    2) Manage Spellbook
+                    3) Manage Gold
+                    4) Manage Level
+
+
+
+                    0) Delete Character
     """)
     
     choice = input("Choose Option: ")
+    if choice == '3':
+        print(f'''
+              {character.name}
+
+              Current Gold: {character.gold} gp
+
+              1) Add
+              2) Remove
+
+
+              
+        ''')
+        change = input("Choose Option: ")
+        if change == '1':
+            new_gp = int(character.gold) + int(input('Gold to add >>> '))
+            character.gold = new_gp
+            session.commit()
+            character_menu(char_id, current_user_id)
+        if change == '2':
+            new_gp = int(character.gold) - int(input('Gold to Remove >>> '))
+            if new_gp >= 0:
+                character.gold = new_gp
+                session.commit()
+                character_menu(char_id, current_user_id)
+            else:
+                print('Insufficient funds')
+                time.sleep(2)
+                character_menu(char_id, current_user_id)
+
     if choice == '0':
-        selected_character = session.query(Character).filter(Character.character_id == id)
+        selected_character = session.query(Character).filter(Character.character_id == char_id)
         confirm = input(f'{selected_character[0].name} will be deleted. (type DELETE to confirm): ')
         if confirm == 'DELETE':
             selected_character.delete()
